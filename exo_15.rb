688ad1658706a6124234183d926946aca965b39f
ndiki931 <kidibrahima@gmail.com>
2020-04-07 21:34:54 +0200

    exo15 added

---
+++ exo_15.rb
@@ -0,0 +1,4 @@
+#ce programme demande l'année de naissance du user et affiche chaque année depuis son année jusqu'au aujourd'hui
+#pour chaque année le programme affiche l'age que l'utlisateur avait à cette année
+
+
